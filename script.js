2bd6b032064b66807065a6551f5c049f8fbe159b
benleong94 <bendemeerben@gmail.com>
2023-10-21 09:37:10 +0800

    Tested the script.js

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+let carddeck = JSON.parse(localStorage.getItem('deck_of_cards'));
+console.log(carddeck[2]);
+
+var button = document.querySelector("#submit-button");
+button.addEventListener("click", function () {
+  var input = document.querySelector("#input-field");
+  var result = main(input.value);
+  var output = document.querySelector("#output-div");
+  output.innerHTML = result;
+
+  input.value = "";
+});
+
 var main = function (input) {
   var myOutputValue = 'hello world';
   return myOutputValue;
 };
+
