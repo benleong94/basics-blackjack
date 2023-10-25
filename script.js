5b237784b5170126a19b2f70cd7cd35385bc4839
benleong94 <bendemeerben@gmail.com>
2023-10-25 17:20:12 +0800

    Added feature to validate numbers

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 // Global Variables
-var players = []
-var winList = []
-var loseList = []
-var carddeck, dealButton, hitButton, standButton, revealButton, output
+var players = [];
+var winList = [];
+var loseList = [];
+var carddeck, dealButton, hitButton, standButton, revealButton, output, playerNum, cardOutput;
+var playerNum = 2; 
+var playerTurn = 0; 
 
 // Functions 
 var initializeDeck = function () {
 
-  var cardDeck = [];
+  carddeck = [];
   var suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
 
   var suitIndex = 0;
@@ -32,30 +34,27 @@ var initializeDeck = function () {
         rank: rankCounter,
       };
 
-      cardDeck.push(card);
+      carddeck.push(card);
       rankCounter += 1;
     }
 
     suitIndex += 1;
   }
-
-  carddeck = cardDeck;
 };
 
 var initializePlayers = function(playerNum){
   for (let i = 1; i <= playerNum; i++) {
+    var player = {
+      name: "",
+      playerHand: [],
+      currentScore: [],
+      status: ""
+    };
+
     if(i == playerNum){
-      var player = {
-        name: "Computer",
-        playerHand: [],
-        currentScore: []
-      };
+      player.name = "Computer";
     } else {
-      var player = {
-        name: "Player #" + i,
-        playerHand: [],
-        currentScore: []
-      };
+      player.name = "Player #" + i;
     }    
     players.push(player);
   }
@@ -70,32 +69,29 @@ var initializeButtons = function(){
   output = document.querySelector("#output-div");
 
   dealButton.addEventListener("click", function () {
-    var cardOutput = "";
+    cardOutput = "";
+
     if (carddeck.length == 0){
       cardOutput = "No Cards Left!"
     } else {
-
       dealFirstHand();
-
-      for (let i=0; i<players.length; i++){
-        var playerName = "<b>" + players[i].name + " Hand </b>"; 
-        players[i].currentScore = calculateHand(players[i]);
-        var playerHandOutput = listOutHand(players[i]);
-        cardOutput += playerName + ": <br>" + playerHandOutput + "<b> Score(s): </b>" + players[i].currentScore + "<br><br>";    
-      };
-
-      if(checkWinOrLose() == true) {
-        cardOutput += "Winners: " + winList
-        output.innerHTML = cardOutput;
-        resetGame();
-      } else {
-        output.innerHTML = cardOutput;
-        resetGame();     
-      }
+      displayFullList(); 
+      validationMessage();
     }
   })
 
-  disableCase(1); 
+  hitButton.addEventListener("click", function () {    
+    cardOutput = "";
+    players[playerTurn].status = "Hit"; 
+    dealHit();
+    displayFullList();
+    validationMessage();
+  })
+
+  standButton.addEventListener("click", function () {
+    cardOutput = "";
+
+  })
 };
 
 var disableCase = function(num){
@@ -146,12 +142,18 @@ var dealFirstHand = function () {
     var cardOne = chooseRandomCard(); 
     var cardTwo = chooseRandomCard();
     players[i].playerHand.push(cardOne);
-    players[i].playerHand.push(cardTwo);          
+    players[i].playerHand.push(cardTwo);      
   }
 }
 
 var dealHit = function () {
-
+  var pickCard = chooseRandomCard();
+  players.forEach(player => {
+    if(player.status == "Hit"){
+      player.playerHand.push(pickCard);
+      player.status = ""; 
+    }
+  })
 }
 
 var checkCards = function () {
@@ -168,6 +170,16 @@ var listOutHand = function (player) {
     hand += player.playerHand[i].name + " of " + player.playerHand[i].suit + "<br>"
   }
   return hand; 
+}
+
+var displayFullList = function () {
+  for (let i=0; i<players.length; i++){
+    var playerName = "<b>" + players[i].name + " Hand </b>"; 
+    players[i].currentScore = calculateHand(players[i]);
+    var playerHandOutput = listOutHand(players[i]);
+    cardOutput += playerName + ": <br>" + playerHandOutput + "<b> Score(s): </b>" + players[i].currentScore + 
+                  "<br> Chosen Score To Display: " + selectBestNumber(players[i].currentScore) + "<br><br>";    
+  };
 }
 
 var calculateHand = function (player) {
@@ -192,34 +204,88 @@ var calculateHand = function (player) {
   return Array.from(scores); 
 }
 
-var checkWinOrLose = function() {
-  var check = false; 
+var checkWin = function() {
+  var checkWinResult = false; 
   for (let i=0; i<players.length; i++){
     players[i].currentScore.forEach(score => {
       if (score == 21) {
         winList.push(players[i].name);
-        check = true; 
-      } else if (score > 21) {
-        loseList.push(players[i].name)
-        check = true;
+        checkWinResult = true; 
       } 
     })
   };
-  return check;
+  return checkWinResult;
+}
+
+var checkLose = function() {
+  checkLoseResult = false; 
+  for (let i=0; i<players.length; i++){
+    if(checkforNumberNotLargerThan21(players[i].currentScore)){
+    } else {
+      checkLoseResult = true; 
+      loseList.push(players[i].name);
+    }
+  };
+  return checkLoseResult;
+}
+
+var checkforNumberNotLargerThan21 = function (array) {
+  var check = false;  
+  array.forEach(element => {
+    if (element <= 21) {
+      check = true;
+    }
+  });
+  return check; 
+}
+
+var selectBestNumber = function (scores) {
+  max = 0; 
+  scores.forEach(score => {
+    if (score == 21) {
+      max = score
+      return max
+    } else if (score > 21) {
+      //pass
+    } else if (score > max) {
+      max = score; 
+    }
+  })
+  return max; 
+}
+
+var validationMessage = function() {
+  if(checkWin() == true) {
+    cardOutput += winList + " wins!"  
+    output.innerHTML = cardOutput;
+    resetGame();
+  } else if (checkLose() == true) {
+    cardOutput += loseList + " loses!"
+    output.innerHTML = cardOutput;
+    resetGame();
+  } else {
+    cardOutput += players[playerTurn].name + ", please select <b>'Hit'</b> to draw another card or <b>'Stand'</b> to move to next player's turn."
+    output.innerHTML = cardOutput;
+    disableCase(2);     
+  }
 }
 
 var resetGame = function() {
   players = []
   winList = []
   loseList = []
-  main(2);
+  playerTurn = 0; 
+  cardOutput = "";
+  initializeDeck();
+  initializePlayers(playerNum);
+  disableCase(1); 
 }
 
-var main = function(playerNum)
-{
+var main = function (playerNum) {
   initializeDeck();
   initializePlayers(playerNum);
   initializeButtons();
+  disableCase(1); 
 }
 
-main(2);
+main(playerNum)
